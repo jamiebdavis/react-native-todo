c53d22972c0b56fd05955d123123d0b618e87ca2
jamiebdavis <jamied1985@gmail.com>
2020-02-03 20:49:58 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    }
+});
